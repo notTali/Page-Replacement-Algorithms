4e80df33df2bee527dac77015b78d448249b4667
notTali <mtalifhani54@gmail.com>
2022-04-20 11:02:36 +0200

    Task 3

---
+++ task3.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Console.Write("Enter your nickname: ");
+            string nickname = Console.ReadLine();
+            Console.Write("Enter your favourite series/movie: ");
+            string movie = Console.ReadLine();
+
+            Console.WriteLine("\nHello World\n" + displayDetails(nickname, movie) + "\n");
+        }
+    }
+
+    static string displayDetails(string nickname, string movie)
+    {
+        string details = "Name     : " + nickname + "\n";
+        details = details + "Favourite: " + movie;
+
+        return details;
+    }
+}
