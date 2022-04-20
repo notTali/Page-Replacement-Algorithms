b8348cd85731cd45225eefc977527e336753c2a6
notTali <mtalifhani54@gmail.com>
2022-04-20 12:25:15 +0200

    Task 1 updated

--- task1.cs
+++ task1.cs
@@ -5,23 +5,20 @@ public class Program
     public static void Main()
     {
 
-        displayDetails();
-
-    }
-
-    static void displayDetails()
-    {
-
         Console.Write("Enter your nickname: ");
         string nickname = Console.ReadLine();
         Console.Write("Enter your favourite series/movie: ");
         string movie = Console.ReadLine();
 
-        Console.WriteLine("\nHello World");
+        Console.WriteLine("\nHello World\n" + displayDetails(nickname, movie) + "\n");
 
+    }
+
+    static string displayDetails(string nickname, string movie)
+    {
         string details = "Name     : " + nickname + "\n";
         details = details + "Favourite: " + movie;
 
-        Console.WriteLine(details);
+        return details;
     }
 }
